25fd0ab2bcc2bb7256a8e879f66d563577b4f4c6
Piotr Boczar <boczarpiotr@gmail.com>
2021-04-08 11:37:11 +0200

    Initial commit

---
+++ src/main/java/com/boczar/gender_detection/GenderDetectionApplication.java
@@ -0,0 +1,13 @@
+package com.boczar.gender_detection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenderDetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenderDetectionApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/boczar/gender_detection/HomeController.java
@@ -0,0 +1,14 @@
+package com.boczar.gender_detection;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+}
