8f8c302cd6b46a8cd8ad3447ee311b458b0bb4ff
Piotr Boczar <boczarpiotr@gmail.com>
2021-04-14 10:22:41 +0200

    "readFromTxtAndCheckAllNames" method added. "detectGenderByAllNames" changed.

--- src/main/java/com/boczar/gender_detection/GenderDetector.java
+++ src/main/java/com/boczar/gender_detection/GenderDetector.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 @Service
 public class GenderDetector {
 
-    public String readFromTxt(String fileName, String gender, String[] names) {
+    public String readFromTextAndCheckFirstName(String fileName, String gender, String[] names) {
         String output = "";
         String currentLine;
         try (BufferedReader bf = new BufferedReader(new FileReader(fileName))) {
@@ -24,52 +24,47 @@ public class GenderDetector {
         return output;
     }
 
+    public int readFromTxtAndCheckAllNames(String fileName, String[] names) {
+        int occurance = 0;
+
+        try (
+                BufferedReader bf = new BufferedReader(new FileReader(fileName))) {
+            String currentLine;
+            while ((currentLine = bf.readLine()) != null) {
+                for (String name : names) {
+                    if (name.equals(currentLine)) {
+                        occurance++;
+                    }
+                }
+            }
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+        return occurance;
+    }
+
 
     public String detectGenderByFistName(String[] names) {
         if (names == null) {
             throw new NullPointerException("Please provide list of names");
         }
-        if ((readFromTxt("male_names.txt", "Male", names)).length() == 0) {
-            return readFromTxt("female_names.txt", "Female", names);
+        if ((readFromTextAndCheckFirstName("male_names.txt", "Male", names)).length() == 0) {
+            return readFromTextAndCheckFirstName("female_names.txt", "Female", names);
         } else {
-            return readFromTxt("male_names.txt", "Male", names);
+            return readFromTextAndCheckFirstName("male_names.txt", "Male", names);
         }
 
     }
+
 
     public String detectGenderByAllNames(String[] names) {
         if (names == null) {
             throw new NullPointerException("Please provide list of names");
         }
+        int maleOccurance = readFromTxtAndCheckAllNames("male_names.txt", names);
+        int femaleOccurance = readFromTxtAndCheckAllNames("female_names.txt", names);
 
-        int maleOccurance = 0;
-        int femaleOccurance = 0;
-
-        try (BufferedReader bf = new BufferedReader(new FileReader("male_names.txt"))) {
-            String currentLine;
-            while ((currentLine = bf.readLine()) != null) {
-                for (String name : names) {
-                    if (name.equals(currentLine)) {
-                        maleOccurance++;
-                    }
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try (BufferedReader bf = new BufferedReader(new FileReader("female_names.txt"))) {
-            String currentLine;
-            while ((currentLine = bf.readLine()) != null) {
-                for (String name : names) {
-                    if (name.equals(currentLine)) {
-                        femaleOccurance++;
-                    }
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
         if (maleOccurance > femaleOccurance) {
             return "Male";
         } else if (femaleOccurance > maleOccurance) {
